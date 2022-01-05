d44431ee5f762b9ebb21ece14d36c77362645581
kalvisz <kalvis.zalans@inbox.lv>
2022-01-05 20:10:37 +0200

    modificēju Test2_search

--- Ebay_test/UnitTest1.cs
+++ Ebay_test/UnitTest1.cs
@@ -7,9 +7,9 @@ using WindowsFormsApp1;
 namespace Ebay_test
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1 
     {
-
+ 
         [TestMethod]
         public void Test1_field()
         {
@@ -29,29 +29,37 @@ namespace Ebay_test
 
             driver.Quit();
 
-            Assert.AreEqual(true, idIr, "ID eksistē");
+            Assert.AreEqual(true, idIr, "ID neeksistē");
 
         }
         [TestMethod]
         public void Test2_search()
         {
-            IWebDriver driver;
-            string path;
-            bool idIr;
-            // ==================================== ceļš uz selenium
-            path = @"C:\Users\Kalvis\Desktop\Studijas\kursi\7.semestris\Programmaturas izstrades tehnologijas\BrowserDrivers";
-            var options = new ChromeOptions();
-            // palaižam chrome bez vizuālās daļas
-            options.AddArguments("headless");
-            options.AddExcludedArgument("enable-logging");
-            driver = new ChromeDriver(path, options);
-            driver.Url = "https://www.ebay.com/";
-            // meklē nepieciešamo ID
-            idIr = driver.FindElement(By.Id("gh-btn")).Displayed;
+            //pēdējā mirklī izdomāju modificēt testu, kas varbūt būtu labāks, nekā pirmais
+            try
+            {
+                IWebDriver driver;
+                string path;
+                bool idIr;
+                // ==================================== ceļš uz selenium
+                path = @"C:\Users\Kalvis\Desktop\Studijas\kursi\7.semestris\Programmaturas izstrades tehnologijas\BrowserDrivers";
+                var options = new ChromeOptions();
+                // palaižam chrome bez vizuālās daļas
+                options.AddArguments("headless");
+                options.AddExcludedArgument("enable-logging");
+                driver = new ChromeDriver(path, options);
+                driver.Url = "https://www.ebay.com/";
+                // meklē nepieciešamo ID
 
-            driver.Quit();
+                idIr = driver.FindElement(By.Id("gh-btn")).Displayed;
 
-            Assert.AreEqual(true, idIr, "ID eksistē");
+                driver.Quit();
+            }catch(OpenQA.Selenium.NoSuchElementException e)
+            {
+                StringAssert.Contains(e.Message, "Id neeksistē");
+                return;
+            }
+            
         }
     }
 }
